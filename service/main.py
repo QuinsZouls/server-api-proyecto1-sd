d03c6a6bc949d93af82a470617e693f53d79cb26
Alfredo Medrano <alfredomedranosanchez@gmail.com>
2021-09-06 11:08:12 -0500

    Add logs

--- service/main.py
+++ service/main.py
@@ -1,8 +1,9 @@
 import socket
 import json
-import io
+import sys
+from cryptography.fernet import InvalidToken
 from utils.crypto import decryptImage
-from utils.socket import socketCatchMessaje, recvall
+from utils.socket import recvall
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 serversocket.bind(("localhost", 6791))
@@ -10,16 +11,25 @@ serversocket.bind(("localhost", 6791))
 serversocket.listen(5)
 
 while True:
-    # accept connections from outside
-    (clientsocket, address) = serversocket.accept()
-    # now do something with the clientsocket
-    # in this case, we'll pretend this is a threaded server
-    request = recvall(clientsocket)
-    parsedReq = json.loads(request.decode())
-
-    if parsedReq['option'] == 'processImage':
-        response = json.dumps({
-            "image": decryptImage(parsedReq['data']).decode(),
-            "type": "result"
-        })
-        clientsocket.send(response.encode())
+    try:
+        # accept connections from outside
+        (clientsocket, address) = serversocket.accept()
+        # now do something with the clientsocket
+        # in this case, we'll pretend this is a threaded server
+        request = recvall(clientsocket)
+        parsedReq = json.loads(request.decode())
+        print('Se ha recibido un mensaje')
+        if parsedReq['option'] == 'processImage':
+            print('Decodificando mensaje...')
+            response = json.dumps({
+                "image": decryptImage(parsedReq['data']).decode(),
+                "type": "result"
+            })
+            clientsocket.send(response.encode())
+            print('Se ha enviado el mensaje')
+    except InvalidToken:
+        print("El token es invalido")
+    except KeyboardInterrupt:
+        sys.exit()
+    except:
+        print('Error desconocido')
